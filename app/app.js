a02b5a30241ddafcddfe06efdf783a65bcb57124
Sunilkumar <bhois0648@gmail.com>
2025-05-31 13:46:00 +0530

    update

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-angular.module('chatApp', [])
+angular.module('chatApp', ['ngRoute'])
 .run(['themeService', function(themeService) {
     // Initialize theme from localStorage or system preference
     themeService.initTheme();
